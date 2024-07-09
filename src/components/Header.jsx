94fa74d16824a33c5209ba34cd707ff582885c2a
Vigneshwaran J <92634319+Vijayadhi@users.noreply.github.com>
2024-07-09 22:48:33 +0530

    background image added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
     return (
         <>
 
-            <header className="masthead">
+            <header className="masthead" style={{background-image: url("https://i.pinimg.com/originals/65/80/2d/65802d8968234537dda78cbe95295160.jpg")}}>
                 <div className="container position-relative">
                     <div className="row justify-content-center">
                         <div className="col-xl-6">
